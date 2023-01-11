d00462bfafacc43ea3ff9ddda7b6699328907e0a
teacher_cheater <johnnykayfun@gmail.com>
2023-01-11 11:39:05 +0300

    get data from inputs and output their value

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-const rangeInput = document.querySelectorAll('.range-input input');
-const formInput = document.querySelectorAll('.form-input input');
-const progressBar = document.querySelector('.slider-first .progress-first');
 
+const rangeInput = document.querySelectorAll('.range-input input');
+const formInput = document.querySelectorAll('.form-elements input');
+const progressBar = document.querySelector('.slider-first .progress-first');
+const formElem = document.querySelector('.form-elements');
+const buttonSubmit = document.querySelector('.button');
 const gap = 1000;
 
 rangeInput.forEach(input => {
   input.addEventListener('input', (event) => {
-    const minInptValue = parseInt(rangeInput[0].value)
-    const maxInptValue = parseInt(rangeInput[1].value)
-
+    const minInptValue = parseFloat(rangeInput[0].value)
+    const maxInptValue = parseFloat(rangeInput[1].value)
 
     if (maxInptValue - minInptValue < gap) {
       if (event.target.className === "range-min") {
-        parseInt(rangeInput[0].value = maxInptValue - gap);
+        parseFloat(rangeInput[0].value = maxInptValue - gap);
       } else {
-        parseInt(rangeInput[1].value = maxInptValue + gap);
-
+        parseFloat(rangeInput[1].value = maxInptValue + gap);
       }
-    } else {
-      formInput[0].value = minInptValue;
-      formInput[1].value = maxInptValue;
-      progressBar.style.left = `${(minInptValue / rangeInput[0].max) * 100}%`;
-      progressBar.style.right = `${100 - (maxInptValue / rangeInput[1].max) * 100}%`;
-
+      return;
     }
-
-    console.log(minInptValue, maxInptValue);
+    formInput[0].value = minInptValue;
+    formInput[1].value = maxInptValue;
+    progressBar.style.left = `${(minInptValue / rangeInput[0].max) * 100}%`;
+    progressBar.style.right = `${100 - (maxInptValue / rangeInput[1].max) * 100}%`;
 
   })
 })
 
 //-----------
-formInput.forEach(input => {
+const resultInput = formInput.forEach(input => {
   input.addEventListener('input', (event) => {
-    const minInptValue = parseInt(formInput[0].value)
-    const maxInptValue = parseInt(formInput[1].value)
+    const minInptValue = parseFloat(formInput[0].value)
+    const maxInptValue = parseFloat(formInput[1].value)
 
 
     if ((maxInptValue - minInptValue >= gap) && maxInptValue <= 10_000) {
       if (event.target.className === "input-min") {
-        parseInt(rangeInput[0].value = minInptValue);
+        parseFloat(rangeInput[0].value = minInptValue);
         progressBar.style.left = `${(minInptValue / rangeInput[0].max) * 100}%`;
 
       } else {
 
-        parseInt(rangeInput[1].value = maxInptValue);
+        parseFloat(rangeInput[1].value = maxInptValue);
         progressBar.style.right = `${100 - (maxInptValue / rangeInput[1].max) * 100}%`;
 
       }
@@ -52,3 +49,27 @@ formInput.forEach(input => {
 
   })
 })
+
+//-----отправка формы на бэк - не используется, сделан вывод в alert
+formElem.onsubmit = async (e) => {
+  e.preventDefault();
+
+  const url = '/....'
+  const response = await fetch(url, {
+    method: 'POST',
+    body: new FormData(formElem)
+
+  });
+
+  const result = await response.json();
+  alert(result.message);
+}
+
+//----вывод значений в alert
+function sendForm(e) {
+  e.preventDefault();
+  alert(`Мин значение: ${parseFloat(rangeInput[0].value)}, Макс значение: ${parseFloat(rangeInput[1].value)}`
+  )
+}
+
+buttonSubmit.addEventListener('click', sendForm);
